02b88c12dd7b440938a8a2de1bb51d9d62e5f6e8
Marwan01 <marwanest@live.fr>
2019-09-18 09:50:56 -0500

    added pug for html rendering

--- website/server/index.js
+++ website/server/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-const routes = require('./routes');
+
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, './views'));
+
 app.use(express.static('public'));
+const routes = require('./routes');
 app.get('/favicon.ico', (req, res, next) => {
     return res.sendStatus(204);
 })
