addb09e98464b8ebedd7b391f62c70e7a2e9073c
Marwan01 <marwanest@live.fr>
2019-09-18 14:08:31 -0500

    added custom error handling for unused routes

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const createError = require('http-errors')
 const path = require('path');
 
 const app = express();
@@ -14,6 +15,17 @@ app.get('/favicon.ico', (req, res, next) => {
 })
 app.use('/', routes());
 
+app.use((req, res, next) => {
+    return next(createError(404, 'file not found'));
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    const status = err.status || 500;
+    res.locals.status = status;
+    res.status(status);
+    return res.render('error');
+});
 app.listen(3000);
 
 module.export = app;
